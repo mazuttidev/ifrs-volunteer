4dfddcaef2afc0f9a35f7a999062f49a270ae4f9
hosvictor <victor.mazuttidev@gmail.com>
2025-09-19 21:57:49 -0300

    feat(footer): criação de layout footer

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,12 @@
+export function Footer() {
+  return (
+    <footer className="mt-12 p-6 border-t bg-gray-100 dark:bg-gray-900 text-center space-y-2">
+      <p>Seu tempo é o presente mais valioso que você pode oferecer.</p>
+      <div className="flex justify-center gap-4">
+        <a href="#">Redes sociais</a>
+        <a href="#">Contato</a>
+        <a href="#">Sobre</a>
+      </div>
+    </footer>
+  );
+}
