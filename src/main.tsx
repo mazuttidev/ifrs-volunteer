b982c4700d5c4887c413315a5a6388c20c432272
hosvictor <victor.mazuttidev@gmail.com>
2025-09-19 21:10:03 -0300

    Corrigindo conflitos

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import LoginPage from './pages/LoginPage'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <LoginPage />
-  </StrictMode>,
-)
-=======
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
@@ -20,4 +8,3 @@ createRoot(document.getElementById('root')!).render(
         <App />
     </StrictMode>
 );
->>>>>>> 4f63089 (feat: criação de navbar + esqueleto das páginas)
